ac93212637120c776833aaacab167f9d48003520
gunnchawla2711 <gunnchawla2711@gmail.com>
2025-08-27 15:33:59 +0530

    Add files via upload

---
+++ ques1.c
@@ -0,0 +1,16 @@
+//write a program to input two numbers and display their sum//
+
+#include<stdio.h>
+
+int main(){
+    int a,b,sum;
+    printf("enter the value of a and b");
+    scanf("%d %d",&a, &b);
+
+    sum=a+b;
+
+    printf("sum=%d",sum);
+
+    return 0;
+    
+}
